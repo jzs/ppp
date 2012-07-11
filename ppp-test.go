0cd49f6d29e16cb7c69b3f5f3b033422f1d7a9fc
jzs jens <jzs@users.noreply.github.com>
2012-07-11 20:12:33 +0200

    Initial import

---
+++ ppp-test.go
@@ -0,0 +1,51 @@
+// Sequence Key:
+// 66F24B34AE4D26DADA87ABF799B247AABE389D5E02E08622D9E0F70C6A44E061
+
+// Character Set:
+// !#%+23456789:=?@ABCDEFGHJKLMNPRS
+// TUVWXYZabcdefghijkmnopqrstuvwxyz
+
+// Passcode length:
+// 4
+
+// First Passcard to generate:
+// 1
+
+// PPP Passcard Demo                  [1]
+//     A    B    C    D    E    F    G
+//  1: AWmL izmn ?eb: zNvm AX9R us+2 F?#F
+//  2: JCDV cLkM V%zY 9PHF 5?xt +mK! MyDT
+//  3: DE!G 4Ftj Zywg 6Nxd buGf :AP8 ub?n
+//  4: snTq WRyD ytCc @7YE 5eHX !9ps ChpS
+//  5: SeC+ uy2N =Pzk jS6d ?zat +Em2 5qsz
+//  6: cjX+ YuRo Smr2 CBLq UKxp UEZ4 #x8i
+//  7: 3G#7 Vv7j ?cJ6 db9r i8MB b6Bd G65n
+//  8: u%62 8LrP F6:A #wPg hYK! MhSf #SCr
+//  9: fVUF MSNy 3k%5 6eT8 +kdJ vMx9 8c@X
+// 10: !JvY @v6g Sy27 5tFh nSxN PXYz 9%5A
+
+// PPP Passcard Demo                  [2]
+//     A    B    C    D    E    F    G
+//  1: 4wED Rv+L 4FAf KB8C VZq2 rYxw +93T
+//  2: qk5x F5Ew Rg9h j#TK r?5v ZRPN WFAz
+//  3: @5!S qpDc NLEG rWZo yV=y KRoD rr5D
+//  4: bqmz Cc?s 3FhR dpr@ 99Ld WZ+X 7?:d
+//  5: Gzd2 mVjv PnCZ =S5W %MMY yi2j 6rjt
+//  6: 9+gL GwWv P7ep qu9j ubAw #sAp 9tYL
+//  7: xV!r PC6e dq5S zE?A 9du7 9ayw pzq2
+//  8: t@Aw MXck 8HLj XfpB 3t3R bHU2 #g29
+//  9: jY=X G8Ez 5qGA ohsC dqxA Gd8C rYv6
+// 10: irVe 2zr6 vn!R FF@Y g2Tn @RpP S+bU
+
+// PPP Passcard Demo                  [3]
+//     A    B    C    D    E    F    G
+//  1: JEiE =W:w iX=j b6kS #md5 @5aa =K%Z
+//  2: NkYT KLcA 8FSM X6Jn cqvi 4?s# nATF
+//  3: vhcm JizR +joS 8=Zn oZ9d +o6S 93Ht
+//  4: =yfi u2dc ZASY f9Ro P:uR 9V!? 3No9
+//  5: TLvS 4kXX MDrp T+A7 hZoC fCcT RWgm
+//  6: HK?k 7wsw U@fY rvJo bE3N t8JW ioSk
+//  7: ZBbj DKhe 4S=Y X@#q =LuK 3C3g R+S7
+//  8: CN4: umAr aG5j 3z:X kAEH FPKY L!qt
+//  9: fxn2 fWgk AENU zJTq jowD oEZP @wpV
+// 10: B=at !fev bgGW oH#A gNVd K8XU 2U=o
